dfe930e5078cc8cac7b42c42168ee18127e30921
Vishwa <gvishwa1997@gmail.com>
2018-03-04 17:45:40 +0530

    Create occurence_of_vowels.c

---
+++ occurence_of_vowels.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a[200]={0},i;
+	char s[100],c[]="AEIOUaeiou";
+	scanf("%[^\n]",s);
+	for(i=0;s[i]!='\0';i++)
+	{
+		a[s[i]]++;
+	}
+	for(i=0;c[i]!='\0';i++)
+	{
+		if(a[c[i]]!=0)
+		{
+			printf("%c %d\n",c[i],a[c[i]]);
+		}
+	}
+}
