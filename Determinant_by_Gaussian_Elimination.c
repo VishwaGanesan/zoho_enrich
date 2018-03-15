26f588528e9622a2061753ff0d7d6575ca49c44f
Vishwa <gvishwa1997@gmail.com>
2018-03-15 20:12:47 +0530

    Create Determinant_by_Gaussian_Elimination.c

---
+++ Determinant_by_Gaussian_Elimination.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+	int n,i,j,k,up=1,mp=0;
+	float b[30][30],ele,ans=1;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%f",&b[i][j]);
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=up;j<n;j++)
+		{ 
+			ele=b[j][i]/b[mp][mp];
+			for(k=0;k<n;k++)
+			{
+				b[j][k]=b[j][k]-(b[i][k]*ele);
+			}
+		}
+		up++;
+		mp++;
+	}
+	for(i=0;i<n;i++)
+	{
+		ans*=b[i][i];
+	}
+	printf("%0.f",ans);
+}
