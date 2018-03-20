9b97430448f1fc133a6014632544988674caa41f
Vishwa <gvishwa1997@gmail.com>
2018-03-20 18:50:08 +0530

    Update Base_conversion.c

--- Base_conversion.c
+++ Base_conversion.c
@@ -3,6 +3,7 @@ int main()
 {
 	int n,base,s;
 	int a[100],i,k=0;
+	long int ans=0;
 	scanf("%d %d",&n,&base);
 	while(n>0)
 	{
@@ -12,7 +13,7 @@ int main()
 	}
 	for(i=k-1;i>=0;i--)
 	{
-		printf("%d",a[i]);
-	}
-	
+		ans=(ans*10)+a[i];
+	} 
+	printf("%ld ",ans);
 }
