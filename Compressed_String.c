e13a7369da546e7a86bfc50f6c0c30aaafc40368
Vishwa <gvishwa1997@gmail.com>
2018-03-21 07:47:21 +0530

    Update Compressed_String.c

--- Compressed_String.c
+++ Compressed_String.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-	char s[1000],c,ans[1000];
-	int count[1000];
+	char s[1000],ans[1000],count[30],c;
 	scanf("%s",s);
 	int i=0,counter=0,k=0;
 	while(s[i]!='\0')
@@ -13,15 +13,18 @@ int main()
 			i++;
 			counter++;
 		}
-		ans[k]=c,count[k]=counter;
-		k++;
-	}
-	for(i=0;i<k;i++)
-	{
-		printf("%c",ans[i]);
-		if(count[i]>1)
+		ans[k]=c;
+		ans[k+1]='\0';
+		if(counter==1)
 		{
-			printf("%d",count[i]);
+			k++;
 		}
-	} 
+		else
+		{
+			sprintf(count,"%d",counter); //convert integer to string
+			strcat(ans,count);
+			k=k+strlen(count)+1;
+		}	
+	}
+	printf("%s ",ans);
 }
